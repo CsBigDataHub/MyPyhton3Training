4c22b463842344eb9d60b696ebe2e0e82913e890
CsBigDataHub <kchetan.hadoop@gmail.com>
2018-09-01 18:10:58 -0500

    added logic to Exercise_Files/05 Variables/dictionary.py

--- Exercise_Files/05 Variables/dictionary.py
+++ Exercise_Files/05 Variables/dictionary.py
@@ -17,6 +17,13 @@ def main():
     d2['seven'] = 7
     for k in d2:
         print(k,d2[k])
+    
+    d3 = dict(
+        one=1,two=2,three=3,four=4
+    )
+    v='seven'
+    print(d3.get(v,"other value")) #This will give vale if `V` exists in the dictionary else give the value in the braces
+
 
 
 if __name__ == "__main__": main()
