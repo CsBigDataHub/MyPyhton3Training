056e3f34cff20ec3aee6512ce22010b3fc18febb
CsBigDataHub <kchetan.hadoop@gmail.com>
2018-09-01 18:24:11 -0500

    added code in Exercise_Files/07 Loops/for.py

--- Exercise_Files/07 Loops/for.py
+++ Exercise_Files/07 Loops/for.py
@@ -4,8 +4,14 @@
 
 
 def main():
-    fh = open('lines.txt')
+    fh = open('Exercise_Files/02 Quick Start/lines.txt')
     for line in fh.readlines():
-        print(line)
+        print(line,end='')
+    for index, line in enumerate(fh.readlines()):
+        print(index,line,end='')   
+    s = 'this is a string'
+    for i,c in enumerate(s):
+        print(i,c)
+        if c == 's':print('index {} is an s'.format(i))
 
 if __name__ == "__main__": main()
