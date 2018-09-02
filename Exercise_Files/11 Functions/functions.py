bf37070c7e6bba0a79b7d99f168c1d39431837ce
CsBigDataHub <kchetan.hadoop@gmail.com>
2018-09-02 08:44:58 -0500

    Added code for functions

--- Exercise_Files/11 Functions/functions.py
+++ Exercise_Files/11 Functions/functions.py
@@ -4,9 +4,17 @@
 
 
 def main():
-    testfunc()
+    testfunc1(412,24)
 
+def testfunc(number):
+    print('This is a test function', number)
+
+def testfunc1(number,another=None,onemore=75):
+    print('This is a test function', number,another,onemore)
+
+""" 
+# pass wil pass the empty block of code
 def testfunc():
-    print('This is a test function')
-
+    pass    
+ """
 if __name__ == "__main__": main()
