91afb7b35b61840efb05a74e3b42cd7a85c3a6a4
CsBigDataHub <kchetan.hadoop@gmail.com>
2018-09-01 20:08:36 -0500

    added code for regex

--- Exercise_Files/09 Regexes/regex.py
+++ Exercise_Files/09 Regexes/regex.py
@@ -6,9 +6,11 @@
 import re
 
 def main():
-    fh = open('raven.txt')
+    fh = open('/home/linuxlite/Downloads/Python3 essential Exercise Files/Ex_Files_Python_3_EssT/Exercise_Files/09 Regexes/raven.txt')
     for line in fh:
-        if re.search('(Len|Neverm)ore', line):
+        if  re.search('(Len|Neverm)ore', line):
             print(line, end='')
-
+        match = re.search('(Len|Neverm)ore',line)
+        if match:
+            print(match.group())
 if __name__ == "__main__": main()
