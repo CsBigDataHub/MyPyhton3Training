b45aee6e861cf5629afa2794d27572a1c25aa4af
CsBigDataHub <kchetan.hadoop@gmail.com>
2018-09-01 19:54:58 -0500

    Added list and slices in ops.py

--- Exercise_Files/08 Operators/ops.py
+++ Exercise_Files/08 Operators/ops.py
@@ -5,5 +5,12 @@
 
 def main():
     print("This is the ops.py file.")
-
+    list=[]
+    list[:]=range(10)
+    print(list)
+    print(list[3])
+    print(list[3:9])
+    print(list[3:9:2]) # slice `:` operator in list can have 3 args 1- start index of list,2 -end of list , 3 - step over index
+    list[3:9:2] = (99,99,99) # replacing the slices with 99
+    print(list)
 if __name__ == "__main__": main()
