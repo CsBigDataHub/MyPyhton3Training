e062dcd04f74719b61d5128b7eb5a2ee7972a3bf
CsBigDataHub <kchetan.hadoop@gmail.com>
2018-09-02 09:00:53 -0500

    adeed inclusive_range method

--- Exercise_Files/11 Functions/generator.py
+++ Exercise_Files/11 Functions/generator.py
@@ -5,7 +5,13 @@
 
 def main():
     print("This is the functions.py file.")
-    for i in range(25):
+    for i in inclusive_range(0,25,1) :
         print(i, end = ' ')
+
+def inclusive_range(start,stop,step):
+    i=start
+    while i <= stop:
+        yield i
+        i+=step
 
 if __name__ == "__main__": main()
