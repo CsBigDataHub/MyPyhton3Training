2546a175f17915de1e02825ceaaaa79ffa526802
CsBigDataHub <kchetan.hadoop@gmail.com>
2018-09-03 10:06:24 -0500

    Added dict operation in sql

--- Exercise_Files/16 Databases/databases-create.py
+++ Exercise_Files/16 Databases/databases-create.py
@@ -7,6 +7,7 @@ import sqlite3
 
 def main():
     db = sqlite3.connect('test.db')
+    db.row_factory = sqlite3.Row # to get dict object
     db.execute('drop table if exists test')
     db.execute('create table test (t1 text, i1 int)')
     db.execute('insert into test (t1,i1) values (?,?)',('one',1))
@@ -16,7 +17,9 @@ def main():
     db.commit()
     query = db.execute('select * from test order by t1')
     for rows in query:
-        print(rows)
+       # print(rows)
+        print(dict(rows))   # to get dict object
+        print(rows['t1'],rows['i1']) # to get dict object segregared
     print('This is the databases.py file')
 
 if __name__ == "__main__": main()
