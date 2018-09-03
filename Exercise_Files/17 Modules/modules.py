e6ae8e1cf311e95c188ceaed6c07ab669835f318
CsBigDataHub <kchetan.hadoop@gmail.com>
2018-09-03 10:27:42 -0500

    excersice on modules

--- Exercise_Files/17 Modules/modules.py
+++ Exercise_Files/17 Modules/modules.py
@@ -7,5 +7,29 @@ import sys
 
 def main():
     print('Python version {}.{}.{}'.format(*sys.version_info))
+    print(sys.platform)
+
+    import os
+    print(os.name)
+    print(os.getenv('PATH'))
+    print(os.getcwd())
+
+    import random
+    x = list(range(25))
+    print(x)
+    random.shuffle(x)
+    print(x)
+
+    import datetime
+    now = datetime.datetime.now()
+    print(now)
+    print(now.year, now.month, now.day, now.hour, now.minute, now.second, now.microsecond)
+    
+""" 
+    import urllib.request
+    page = urllib.request.urlopen('http://google.com')
+    for line in page: print(str(line, encoding= 'utf_8'), end='')
+ """
+
 
 if __name__ == "__main__": main()
