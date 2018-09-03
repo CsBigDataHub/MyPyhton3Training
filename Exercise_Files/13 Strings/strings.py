79ca987007a7b215ecf34819679faaabb141b283
CsBigDataHub <kchetan.hadoop@gmail.com>
2018-09-03 09:00:27 -0500

    added lines in strings

--- Exercise_Files/13 Strings/strings.py
+++ Exercise_Files/13 Strings/strings.py
@@ -17,4 +17,16 @@ def main():
     print(s.isdigit())
     print(s.isprintable())
 
+    #format replace
+    d= dict(bob='a',fred='b')
+    s= 'This is {bob} and that is {fred}'.format(**d)
+    print(s)
+
+    split_s = s.split()
+    for w in split_s : print(w)
+
+    new_s = ";".join(split_s)  
+
+    print(new_s)  
+
 if __name__ == "__main__": main()
