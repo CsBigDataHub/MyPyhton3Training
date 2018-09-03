5ed05f1680d933a6816a7462f59ad7ee72e9003d
CsBigDataHub <kchetan.hadoop@gmail.com>
2018-09-03 09:21:02 -0500

    Added code to handle byte code and utf-8

--- Exercise_Files/14 Containers/containers.py
+++ Exercise_Files/14 Containers/containers.py
@@ -4,6 +4,17 @@
 
 
 def main():
-    print('This is the containers.py file.')
+    print('This is the containers.py file. to convert byte code')
+    fileIn = open('Exercise_Files/14 Containers/utf8.txt', mode='r', encoding='utf-8')
+    fileOut = open('Exercise_Files/14 Containers/utf8-decoded.html', mode ='w')
+    outbytes = bytearray()
+    for line in fileIn:
+        for word in line:
+            if ord(word) > 127:
+                outbytes += bytes('&#{:04d};'.format(ord(word)), encoding='utf-8')
+            else: outbytes.append(ord(word))
+    
+    outStr = str(outbytes, encoding='utf-8')
 
+    print(outStr,file=fileOut)
 if __name__ == "__main__": main()
